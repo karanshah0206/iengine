b24b15ccb6abef98fbe72dd23f05091bde92f5ba
RUHAN VENUJA NANAYAKKARA <103522565@student.swin.edu.au>
2022-05-16 23:41:59 +1000

    Add Postfix Evaluator and PLTrue Function To IE Base Class

--- iengine/IE.cs
+++ iengine/IE.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace iengine
 {
@@ -18,6 +20,48 @@ namespace iengine
 
         // Abstract Inference Function
         public abstract void Infer(KB kB, string query);
+
+        // Check If Model Satisfies Knowledge Base
+        protected bool PLTrue(KB kB, Dictionary<string, bool> model)
+        {
+            foreach (Queue<string> sentence in kB.PostfixSentences)
+                if (!PostfixEvaluator(sentence, model))
+                    return false;
+            return true;
+        }
+
+        // Evaluate Propositional Postfix Sentence Using Model
+        protected bool PostfixEvaluator(Queue<string> sentence, Dictionary<string, bool> model)
+        {
+            Stack<bool> stack = new Stack<bool>();
+            bool LHS, RHS;
+
+            while (sentence.Count > 0)
+                // Token Is Symbol
+                if (Regex.IsMatch(sentence.Peek(), "^[a-zA-Z0-9]+$"))
+                    stack.Push(model[sentence.Dequeue()]);
+                // Token Is Operation
+                else
+                    switch (sentence.Dequeue())
+                    {
+                        case "~": // Negation
+                            stack.Push(!stack.Pop()); break;
+                        case "&": // Conjunction
+                            RHS = stack.Pop(); LHS = stack.Pop();
+                            stack.Push(RHS && LHS); break;
+                        case "||": // Disjunction
+                            RHS = stack.Pop(); LHS = stack.Pop();
+                            stack.Push(LHS || RHS); break;
+                        case "=>": // Implication
+                            RHS = stack.Pop(); LHS = stack.Pop();
+                            stack.Push(!LHS || RHS); break;
+                        case "<=>": // Biconditional
+                            RHS = stack.Pop(); LHS = stack.Pop();
+                            stack.Push(LHS == RHS); break;
+                    }
+
+            return stack.Pop();
+        }
 
         // Creator Pattern
         public static IE CreateIE(string mode)
