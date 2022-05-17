8f635c8862ce75cab2bc13d1654a2b4621fc5c4a
Karan Shah <karanshah0206@outlook.com>
2022-05-17 18:59:04 +1000

    Created TT Model Checking Inference Method

--- iengine/TT.cs
+++ iengine/TT.cs
@@ -1,4 +1,6 @@
-﻿namespace iengine
+﻿using System.Collections.Generic;
+
+namespace iengine
 {
     class TT : IE
     {
@@ -6,6 +8,35 @@
         { _output = "0"; }
 
         public override void Infer(KB kB, string query)
-        {}
+        {
+            // Get All Symbols From KB
+            Dictionary<string, bool> symbols = new(kB.Symbols);
+
+            // Get All Symbols From Query
+            foreach (string symbol in KB.GetSymbolsFromSentence(KB.SentenceToArray(query)))
+                if (!symbols.ContainsKey(symbol)) symbols[symbol] = false;
+
+            // Check All Models Using Truth Table Checking
+            TTCheckAll(kB, kB.ShuntingYard(KB.SentenceToArray(query)), new(symbols.Keys), new());
+        }
+
+        // Use Truth Table Checking Method To Enumerate Models
+        private void TTCheckAll(KB kB, Queue<string> query, Queue<string> symbols, Dictionary<string, bool> model)
+        {
+            if (symbols.Count == 0)
+            {
+                // Check If Both Satisfies Both KB & Query
+                if (PLTrue(kB, model) && PostfixEvaluator(query, model))
+                    _output = (int.Parse(_output) + 1).ToString();
+            }
+            else
+            {
+                // Generate Models Using Recursion
+                Dictionary<string, bool> m1 = new(model);
+                string symbol = symbols.Dequeue();
+                m1[symbol] = true; TTCheckAll(kB, new(query), new(symbols), m1);
+                m1[symbol] = false; TTCheckAll(kB, new(query), new(symbols), m1);
+            }
+        }
     }
 }
