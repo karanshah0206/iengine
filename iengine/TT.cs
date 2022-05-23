00c2c5651302453333fef22af795b7375339bf6b
Karan Shah <karanshah0206@outlook.com>
2022-05-23 19:14:09 +1000

    TT Error Fix - Query Must Be True For ALL Models Of KB

--- iengine/TT.cs
+++ iengine/TT.cs
@@ -16,18 +16,27 @@ namespace iengine
             foreach (string symbol in KB.GetSymbolsFromSentence(KB.SentenceToArray(query)))
                 if (!symbols.ContainsKey(symbol)) symbols[symbol] = false;
 
-            // Check All Models Using Truth Table Checking
+            // Check All Models Using Truth Table Model Checking
             TTCheckAll(kB, kB.ShuntingYard(KB.SentenceToArray(query)), new(symbols.Keys), new());
+
+            if (_output == "0") _output = "NO";
+            else if (_output != "NO") _output = "YES: " + _output;
         }
 
         // Use Truth Table Checking Method To Enumerate Models
         private void TTCheckAll(KB kB, Queue<string> query, Queue<string> symbols, Dictionary<string, bool> model)
         {
+            if (_output == "NO") return; // Query Not True For All Models Of KB
+
             if (symbols.Count == 0)
             {
-                // Check If Both Satisfies Both KB & Query
-                if (PLTrue(kB, model) && PostfixEvaluator(query, model))
-                    _output = (int.Parse(_output) + 1).ToString();
+                if (PLTrue(kB, model)) // Model Satisfies KB
+                {
+                    if (PostfixEvaluator(query, model)) // Model Satisfies Query
+                        _output = (int.Parse(_output) + 1).ToString();
+                    else // Query Not True For All Models of KB
+                    { _output = "NO"; return; }
+                }
             }
             else
             {
